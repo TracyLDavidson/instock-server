02e6e6508c05d8e6cb726d82c4696c662903c264
Eric Lo <eric.lo.ca@outlook.com>
2023-09-27 08:39:59 -0700

    merge fixes

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ require("dotenv").config();
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-  client: "mysql",
+  client: process.env.DB_CLIENT,
   connection: {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
